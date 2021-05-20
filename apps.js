bdd9c1f6da406558a08f8f63ee38c47c10209829
Boston Moywaywa <OMAX1139@OMX1139>
2021-05-19 19:10:41 -0700

    init

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('mobile menu');
-const meuLinks = document.querySelector('.navbar__menu');
+const menuLinks = document.querySelector('.navbar__menu');
 menu.addEventListener('click'function() {
     menu.classList.toggle('is-active');
-    menuLinksd.classList.toggle('active');
+    menuLinks.classList.toggle('active');
 
 })
